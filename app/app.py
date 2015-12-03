0b2b3f1b2e858c395cfc8837e044ed69863f226a
dcamerik <dcamerik@umich.edu>
2015-12-02 21:15:17 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -44,6 +44,13 @@ questions = [
          "answer_id" : "2",
          "explanation" : "explanation 2",
     },
+
+    {
+         "type" : "fill_in_the_blanks",
+         "question_text" : "q 3 text",
+         "answer_id" : "answer",
+         "explanation" : "explanation 3",
+    },
 ]
 
 @app.route('/')
@@ -54,13 +61,21 @@ def index():
 def next(qno):
     if (qno < 0 or qno >= len(questions)):
         raise ValueError("invalid question number on request: " + qno);
-
-    resp = make_response(render_template("index.html",
-        mode = "question",
-        question_number = qno,
-        question = questions[qno],
-        check_answer_url = url_for("check_answer")
-    ))
+    
+    if (qno < 2):
+        resp = make_response(render_template("index.html",
+            mode = "mc_question",
+            question_number = qno,
+            question = questions[qno],
+            check_answer_url = url_for("check_answer")
+            ))
+    else:
+        resp = make_response(render_template("index.html",
+            mode = "fb_question",
+            question_number = qno,
+            question = questions[qno],
+            check_answer_url = url_for("check_answer")
+            ))
 
     resp.set_cookie('qno', str(qno))
 
@@ -94,8 +109,27 @@ def check_answer():
             all_done = is_all_done
         )
     elif question_type == "fill_in_the_blanks":
-        # Need to integrate Dana's work here
-        pass
+        is_all_done = "no"
+        if (qno + 1 >= len(questions)):
+            is_all_done = "yes"
+        expected_answer_id = questions[qno]["answer_id"]
+        actual_answer_id = request.form["answerText"]
+        status_value = "incorrect"
+        if expected_answer_id == actual_answer_id:
+            status_value = "correct"
+        return render_template("index.html",
+            mode = "answer",
+            status = status_value,
+            question_number = qno,
+            explanation = questions[qno]["explanation"],
+            next_question_url = url_for("next", qno = (qno+1)),
+            all_done = is_all_done
+            )
+
+
+@app.route('/videos')
+def videos():
+    return render_template("videos.html", name="videos")
 
 if __name__ == '__main__':
     app.run(debug=True)
