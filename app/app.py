86900abaf1f3b668273abaebabdeab8153d5a18b
radhikamani88 <radhikak@umich.edu>
2015-12-10 19:15:56 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,53 +1,125 @@
 from flask import Flask, render_template, url_for, redirect, request, make_response
+from forms import ContactForm
+from flask.ext.mail import Message, Mail
 
 app = Flask(__name__)
+app.secret_key = 'CSSisFun'
 
+app.config.update(dict(
+    DEBUG = True,
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = 'umsiwebdesign@gmail.com',
+    MAIL_PASSWORD = '105sstate',))
+
+mail = Mail(app)
 questions = [
     {
         "type" : "multiple_choice",
-        "question_text" : "q 1 text",
+        "question_text" : "What does CSS stand for?",
         "choices" : [
             {
                 "id" : "1",
-                "text" : "1 choice 1",
+                "text" : "Cascading Style Sheets",
             },
             {
                 "id" : "2",
-                "text" : "1 choice 2",
+                "text" : "Computer Style Sheets",
             },
             {
                 "id" : "3",
-                "text" : "1 choice 3",
+                "text" : "Colorful Style Sheets",
             },
          ],
          "answer_id" : "1",
-         "explanation" : "explanation 1",
+         "explanation" : "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language",
     },
 
     {
         "type" : "multiple_choice",
-        "question_text" : "q 2 text",
+        "question_text" : "Which CSS property controls the text size",
         "choices" : [
             {
                 "id" : "1",
-                "text" : "2 choice 1",
+                "text" : "text-style",
             },
             {
                 "id" : "2",
-                "text" : "2 choice 2",
+                "text" : "font-size",
             },
             {
                 "id" : "3",
-                "text" : "2 choice 3",
+                "text" : "font-height",
             },
          ],
          "answer_id" : "2",
-         "explanation" : "explanation 2",
+         "explanation" : "The font-size property sets the size of the text",
     },
-
+{
+    "type" : "multiple_choice",
+    "question_text" : "Which snippet of CSS is commonly used to center a website horizontally?",
+    "choices" : [
+        {
+            "id" : "1",
+            "text" : "site-align: center; ",
+        },
+        {
+            "id" : "2",
+            "text" : "margin: center; ",
+        },
+        {
+            "id" : "3",
+            "text" : "margin: 0 auto ; ",
+        },
+     ],
+     "answer_id" : "3",
+     "explanation" : "Margin 0 auto helps to horizontally center that element within its container",
+},
+{
+    "type" : "multiple_choice",
+    "question_text" : "In the following code snippet, what value is given for the left margin margin: 5px 10px 3px 8px;",
+    "choices" : [
+        {
+            "id" : "1",
+            "text" : "8px",
+        },
+        {
+            "id" : "2",
+            "text" : "5px",
+        },
+        {
+            "id" : "3",
+            "text" : "10px",
+        },
+     ],
+     "answer_id" : "1",
+     "explanation" : "The margin and padding are defined in this order: Top Right Bottom Left",
+},
+{
+    "type" : "multiple_choice",
+    "question_text" : "Which of the following property specifies which sides of an element other floating elements are not allowed.",
+    "choices" : [
+        {
+            "id" : "1",
+            "text" : "cleared",
+        },
+        {
+            "id" : "2",
+            "text" : "clear",
+        },
+        {
+            "id" : "3",
+            "text" : "float",
+        },
+     ],
+     "answer_id" : "2",
+     "explanation" : "The clear property is used to control the behavior of floating elements.Elements after a floating element will flow around it. To avoid this, use the clear property.",
+},
     {
          "type" : "fill_in_the_blanks",
-         "question_text" : "q 3 text",
+         "question_text" : "q 4 text",
          "answer_id" : "answer",
          "explanation" : "explanation 3",
     },
@@ -61,8 +133,8 @@ def index():
 def next(qno):
     if (qno < 0 or qno >= len(questions)):
         raise ValueError("invalid question number on request: " + qno);
-    
-    if (qno < 2):
+
+    if (questions [qno] ["type"] == "multiple_choice"):
         resp = make_response(render_template("index.html",
             mode = "mc_question",
             question_number = qno,
@@ -131,11 +203,27 @@ def check_answer():
 def videos():
     return render_template("videos.html", name="videos")
 
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    form = ContactForm()
+    if request.method == 'POST':
+        msg = Message(form.subject.data, sender = ("Dana & Radhika", "dcamerik@umich.edu"), recipients = ["dcamerik@umich.edu", "radhikamani88@gmail.com"])
+        msg.body = """
+        FROM: %s <%s>
+        %s
+        """ % (form.name.data, form.email.data, form.message.data)
+        msg_confirm = Message("Confirmation: " + form.subject.data, sender = ("Dana & Radhika", "dcamerik@umich.edu"), recipients = [form.email.data])
+        msg_confirm.body = """
+        From: %s <%s>
+        %s
+        """ % (form.name.data, form.email.data, form.message.data)
+        mail.send(msg)
+        mail.send(msg_confirm)
+        form.reset()
+        return render_template('feedback.html', form=form, name="feedback", mode="sent")
+    elif request.method == 'GET':
+        return render_template('feedback.html', form=form, name="feedback")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
